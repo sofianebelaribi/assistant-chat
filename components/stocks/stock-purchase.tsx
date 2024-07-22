043585f76555a05cf62fa448d675abd9d93f853e
SofianeAM <sofiane.belaribi@arcelormittal.com>
2024-07-22 23:29:39 +0000

    editing actions

--- components/stocks/stock-purchase.tsx
+++ components/stocks/stock-purchase.tsx
@@ -36,7 +36,7 @@ export function Purchase({
     // Insert a hidden history info to the list.
     const message = {
       role: 'system' as const,
-      content: `[User has changed to purchase €{newValue} shares of €{name}. Total cost: €€{(
+      content: `[User has changed to purchase €{newValue} Tons of €{name}. Total cost: €€{(
         newValue * price
       ).toFixed(2)}]`,
 
